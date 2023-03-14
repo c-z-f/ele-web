0f585568a9dd75391d833410f83cde9c2f2bf571
畅智锋 <18295790529@163.com>
2023-03-14 16:09:19 +0800

    添加状态管理配置

--- src/store/module/index.js
+++ src/store/module/index.js
@@ -1,9 +1,13 @@
-const requireStore = require.context('.', false, '/.store.js/')
+const requireStore = require.context('.', false, /.store.js$/)
 
 const modules = {}
+console.log('%c --------开始-------- ', 'background: #222; color: #bada55')
 requireStore.keys().forEach(fileName => {
   const moduleName = fileName.replace(/(\.\/|\.store.js)/g, '')
-  modules[moduleName] = requireStore[fileName].default
+  console.log('%c 模块名称 ' + moduleName, 'background: #666666; color: #F5F5F5')
+
+  modules[moduleName] = requireStore(fileName).default
 })
+console.log('%c --------结束-------- ', 'background: #222; color: #bada55')
 
 export default modules
